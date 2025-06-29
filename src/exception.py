f311c49db270fdb74c2435c53903ecafeefd37c1
mak-1204 <akshay.nitt04@gmail.com>
2025-06-29 15:31:54 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -6,9 +6,7 @@ from src.logger import logging
 
 # Any exception that occurs in the code will be caught here
 def error_message_details(error, error_details):
-    """
-    Print the error message and details.
-    """
+    """Print the error message and details"""
     _, _, exc_tb = error_details
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = f"Error occurred in script: [{file_name}] at line number: [{exc_tb.tb_lineno}] with error message: [{str(error)}]"
